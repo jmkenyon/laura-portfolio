cefa78775cb0ceaa559f08298a6297fc71ae9f21
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-12 10:50:53 -0300

    arrow

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { useRef } from "react";
 import { Navbar } from "./components/Navbar";
 import { Translate } from "./components/Translate";
+import { IoIosArrowDown } from "react-icons/io";
 
 const images: string[] = [
   "/background-image.png",
@@ -8,6 +12,21 @@ const images: string[] = [
 ];
 
 export default function Home() {
+  const sectionRefs = useRef<HTMLDivElement[]>([]);
+
+  const scrollToNext = () => {
+    const currentIndex = sectionRefs.current.findIndex(
+      (el) => el && el.getBoundingClientRect().top === 0
+    );
+
+    const nextIndex =
+      currentIndex === -1 ? 0 : (currentIndex + 1) % sectionRefs.current.length;
+
+    sectionRefs.current[nextIndex]?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
       <div className="hidden sm:block fixed z-50 right-8 top-8">
@@ -17,10 +36,20 @@ export default function Home() {
       <div className="fixed top-13 w-full p-8">
         <Navbar mode="dark" />
       </div>
+      <div
+        onClick={scrollToNext}
+        className="cursor-pointer fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50 animate-bounce text-white"
+      >
+        <IoIosArrowDown size={24} />
+      </div>
+
       <div className="h-screen overflow-y-scroll snap-y snap-mandatory scroll-container">
         {images.map((image: string, index: number) => (
           <div
             key={index}
+            ref={(el) => {
+              if (el) sectionRefs.current[index] = el;
+            }}
             className="snap-start flex items-center justify-center"
             style={{
               backgroundImage: `url(${image})`,
