2338f60edf0bc09abd6ed2d3b7eefa4ab6734926
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-12 12:20:44 -0300

    socials and contact

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Image from "next/image";
+
 import { useState } from "react";
 
 export default function ContactForm() {
@@ -31,18 +31,13 @@ export default function ContactForm() {
   return (
     <div
     id="contact"
-    className="p-4"
+    className="p-8"
   >
-    <h4
-      className="text-center text-lg mb-2 text-gray-600 dark:text-gray-300"
-    >
-      Drop me a message.
-    </h4>
     <h2
 
-      className="text-4xl font-bold text-center mb-8"
+      className="text-sm text-center py-8"
     >
-      Get in touch
+      mande uma mensagem
     </h2>
     <form
       onSubmit={onSubmit}
@@ -54,7 +49,7 @@ export default function ContactForm() {
           className="flex-1 text-black p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white
            dark:border-white/90 "
           type="text"
-          placeholder="Enter your name"
+          placeholder="nome completo"
           required
           name="name"
         />
@@ -62,7 +57,7 @@ export default function ContactForm() {
    
           className="flex-1 text-black p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
           type="email"
-          placeholder="Enter your email"
+          placeholder="e-mail"
           required
           name="email"
         />
@@ -70,19 +65,19 @@ export default function ContactForm() {
       <textarea
 
         rows="6"
-        placeholder="Enter your message"
+        placeholder="mensagem"
         className="text-black w-full p-4 outline-none border-[0.5px]
-      border-gray-400 rounded-md bg-white mb-6 "
+      border-gray-400 rounded-md bg-white mb-6"
         required
         name="message"
       ></textarea>
       <button
 
         type="submit"
-        className="mouse-pointer py-3 px-8 w-max flex items-center justify-between
-      gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"
+        className="cursor-pointer py-3 px-8 w-max flex items-center justify-between
+      gap-2 text-black rounded-full mx-auto duration-500  hover:text-gray-400"
       >
-        Submit
+        enviar
       </button>
       <p className="mt-4">{result}</p>
     </form>
--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 import ContactForm from "../components/ContactForm";
 import { Translate } from "../components/Translate";
+import { PiInstagramLogoThin } from "react-icons/pi";
+import { PiLinkedinLogoThin } from "react-icons/pi";
+import Link from "next/link";
 
 const Page = () => {
   return (
@@ -13,6 +16,20 @@ const Page = () => {
       <Navbar />
 
       <ContactForm />
+
+      <div className="sm:flex-col flex flex-row justify-center gap-2 sm:justify-start">
+        <Link
+          href={"https://www.instagram.com/lauraconsoni.arq"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <PiInstagramLogoThin size={24} />
+        </Link>
+        <Link href={"https://www.linkedin.com/in/lauraconsoni/"} target="_blank" rel="noopener noreferrer">
+
+          <PiLinkedinLogoThin size={24} />
+        </Link>
+      </div>
     </div>
   );
 };
