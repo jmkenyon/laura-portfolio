5fff3ba4d2a8df103dfffc87cfd65880b889c6aa
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-12 12:37:36 -0300

    logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,30 +1,38 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
 
 import { cn } from "../lib/utils";
+import Image from "next/image";
 
 interface NavbarProps {
-    mode?: "light" | "dark";
+  mode?: "light" | "dark";
 }
 
-export const Navbar = ({mode}: NavbarProps) => {
+export const Navbar = ({ mode }: NavbarProps) => {
   return (
     <div>
-
       <div className="text-xs ">
-        <div className={cn("flex flex-row justify-between ",
-                    mode === "dark" ? "text-white" : "text-black"
-                    )} >
-          <div className="flex flex-col text-left ">
-            <Link href="/" className="font-bold">
-              LAURA CONSONI
+        <div
+          className={cn(
+            "flex flex-row justify-between ",
+            mode === "dark" ? "text-white" : "text-black"
+          )}
+        >
+          <div className="flex flex-col">
+            <Link href="/">
+            <Image
+         
+              src="/logo.png"
+              alt="Laura Consoni Logo"
+              width={120}
+              height={40}
+              className="h-auto sm:w-40 w-32"
+            />
             </Link>
           </div>
-          <nav className="justify-end space-x-[46px] pb-4 text-right sm:block hidden"
-
-         >
+          <nav className="justify-end space-x-[46px] pb-4 text-right sm:block hidden">
             <Link href="/sobre-mim">sobre mim</Link>
             <Link href="/projetos">projetos</Link>
             <Link href="/contato">contato</Link>
