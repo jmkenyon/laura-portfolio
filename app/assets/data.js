c455e21d4ac5669ad8736e6821337053da8b6a00
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-14 16:40:00 -0300

    started about section and added cv data

---
+++ app/assets/data.js
@@ -0,0 +1,123 @@
+export const cvData = [
+  {
+    title: "EXPERIÊNCIAS",
+    items: [
+      {
+        text: "Estágio",
+      },
+      {
+        text: "Shinagawa Arquitetura",
+      },
+      {
+        text: "Auxiliar de projetos arquitetônicos",
+      },
+    ],
+  },
+  {
+    title: "ACADÊMICO",
+    items: [
+      {
+        subtitle: "Graduação em Arquitetura e Urbanismo",
+        text: [
+          "Universidade Presbiteriana Mackenzie, São Paulo - Brasil",
+          "2020/1 - atual",
+        ],
+      },
+      {
+        subtitle: "XVIII Jornada Institucional de Iniciação Científica",
+        text: [
+          "“Requalificação de Ruínas: uma análise apartir das teorias de restauro e estudos de caso”",
+          "2021/2 - 2022/1",
+        ],
+      },
+      {
+        subtitle: "Graduação em Arquitetura e Urbanismo",
+        text: [
+          "Universidade Presbiteriana Mackenzie, São Paulo - Brasil",
+          "2020/1 - atual",
+        ],
+      },
+      {
+        subtitle:
+          "III Simpósio Internacional de Ciência, Tecnologia e Inovação",
+        text: [
+          "Palestra ministrada sobre a pesquisa realizada sobre ruínas",
+          "2022/2",
+        ],
+      },
+      {
+        subtitle: "Participação da matéria optativa Urban Space",
+        text: ["Componente ministrado em inglês", "2023/1"],
+      },
+      {
+        subtitle: "Programa Fluxo Contínuo - Intercâmbio",
+        text: ["ENSAP Val-de-Seine, Paris - França", "2023/2 - 2024/1"],
+      },
+      {
+        subtitle: "Projeto selecionado para apresentação",
+        text: [
+          "Apresentação do projeto final S8 (RoissyPôle) para a divisão de arquitetura do Aeroporto Charles de Gaulle",
+          "ENSAP Val-de-Seine, Paris - França",
+          "2024",
+        ],
+      },
+    ],
+  },
+  {
+    title: "IDIOMAS",
+    items: [
+      {
+        text: "Português - nativo",
+      },
+      {
+        text: "Inglês - fluente",
+      },
+      {
+        text: "Francês - básico",
+      },
+    ],
+  },
+  {
+    title: "COMPETIÇÕES",
+    items: [
+      {
+        text: "Participação",
+      },
+      {
+        text: "Tiny House - Volume Zero",
+      },
+      {
+        text: "2022/2",
+      },
+    ],
+  },
+  {
+    title: "PUBLICAÇÕES",
+    items: [
+      {
+        text: "Anais da XVIII Jornada Institucional de Iniciação Científica",
+      },
+      {
+        text: "“Requalificação de Ruínas: uma análise apartir das teorias de restauro e estudos de caso”",
+      },
+      {
+        text: "2022/2",
+      },
+    ],
+  },
+  {
+    title: "SOFTWARES",
+    items: [
+      { text: "Autodesk Autocad" },
+      { text: "Archicad" },
+      { text: "Rhinoceros" },
+      { text: "Sketchup" },
+      { text: "Caos Vray" },
+      { text: "Twinmotion" },
+      { text: "Adobe Photoshop" },
+      { text: "Adobe Indesign" },
+      { text: "Adobe Illustrator" },
+      { text: "Adobe Lightroom" },
+    ]
+  }
+];
--- app/sobre-mim/page.tsx
+++ app/sobre-mim/page.tsx
@@ -1,6 +1,7 @@
-
+import Image from "next/image";
 import { Navbar } from "../components/Navbar";
 import { Translate } from "../components/Translate";
+import Resume from "../components/Resume";
 
 const Page = () => {
   return (
@@ -10,7 +11,34 @@ const Page = () => {
         <Translate mode="light" />
       </div>
       <Navbar />
-      <div>Sobre mim</div>
+      <div className="flex sm:flex-row flex-col">
+        <Image
+          src="/laura-about.jpg"
+          alt="Laura"
+          width={350}
+          height={600}
+          className="lg:ml-30 md:ml-14 sm:ml:8 mt-18  xlg:h-[500px] lg:h-[450px] h-[400px]  w-auto object-cover"
+        />
+        <div className="flex flex-col gap-8 py-18 lg:px-20 md:px-16 sm:px-10 justify-start max-w-lg text-sm">
+          <h1>[sobre mim]</h1>
+        <p>
+          Sou uma estudante de arquitetura, atualmente no 8º semestre na Universidade Presbiteriana Mackenzie, em São Paulo. Tenho 22 anos, sou
+          natural de Santos (SP) e me mudei para São Paulo em 2020 para cursar a
+          faculda- de.
+        </p>
+        <p>
+          Entre 2021 e 2022, participei da XVIII Jornada Institucional de
+          Iniciação Científica, onde submeti um artigo sobre requalificação de
+          ruínas, orien- tado pelo professor Guilherme Mi- chellin, da FAU
+          Mackenzie.
+        </p>
+        <p>
+          Em 2023, realizei um intercâmbio na França na ENSAP Val-de-Seine, em
+          Paris, onde estudei por um ano.
+        </p>
+        </div>
+        <Resume />
+      </div>
     </div>
   );
 };
